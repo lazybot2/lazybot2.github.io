780096a7322e08fd618d86760aa6e1691f882724
lazybot2 <1979428838@qq.com>
2023-02-22 10:37:43 +0800

    Cydia

--- neo/1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install i2c-tools git vim armbian-config python3-dev python3-pil python3-smbus python3-pip python3-serial
-sudo armbian-config
-Menu System > Hardware > enable i2c0
-Save and reboot
